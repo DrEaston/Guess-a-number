34c0a6faafbdcffed0a15036ab7ce45aa53e82b2
CurtisEaston <>
2022-01-11 17:00:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randint(0,10)
+guess=12
+
+while number!=guess:
+  guess=int(input("Guess a number 0-10: "))
+  if guess == number:
+    print("You got it, nice job!")
+  else:
+    print("nope, guess again")
+
